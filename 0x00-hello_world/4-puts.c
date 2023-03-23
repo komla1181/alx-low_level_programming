8c82ca3461834e8ffe08bde2cad118a7d516a1a0
Komla <adabahy@gmail.com>
2023-03-23 13:46:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A line that prints with puts function program
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
