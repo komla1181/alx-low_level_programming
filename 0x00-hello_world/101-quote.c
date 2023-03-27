383b9e7f9d34beb7be92358acb4bf7c1076035ca
Komla <adabahy@gmail.com>
2023-03-27 02:57:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program that prints a line to the standard error
+ * main - C program that prints line to the standard error
  * Return 1 (correct)
  */
 int main(void)
